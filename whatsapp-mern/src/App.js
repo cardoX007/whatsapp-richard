34dbd9c8633122d9136934e3bd00821ac15e653a
unknown <cardoelcapone@gmail.com>
2020-09-22 17:49:10 -0700

    front end

--- whatsapp-mern/src/App.js
+++ whatsapp-mern/src/App.js
@@ -1,24 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from 'react';
+import Sidebar from './Sidebar';
+import Chat from './Chat';
+import './App.css'
+import Pusher from 'pusher-js'
+import axios from "./axios"
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [messages, setMessages] =  useState([]);
+
+  useEffect(() => {
+   
+    axios.get('/messages/sync')
+      .then((response)=>{
+        setMessages(response.data);
+      })
+  }, [])
+
+  useEffect(() => {
+    const pusher = new Pusher('213c701d4f851b7e4bed', {
+      cluster: 'us3'
+    });
+
+    const channel = pusher.subscribe('messages');
+    channel.bind('inserted', function(newMessage) {
+      
+      setMessages([...messages,newMessage ]);
+    });
+    return()=>{
+      channel.unbind_all();
+      channel.unsubscribe();
+    }
+  }, [messages]); 
+    
+  console.log(messages);
+    return (
+    <div className="app">
+      <div className="app__body">
+        <Sidebar />
+        <Chat messages={messages}/>
+          
+      </div>
+         
+          
     </div>
   );
 }
---
+++ whatsapp-mern/src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAi0Kbst-kY8gg7WlwVi3Bjmd08ftRGSVA",
+    authDomain: "whatsapp-mern-1d8b7.firebaseapp.com",
+    databaseURL: "https://whatsapp-mern-1d8b7.firebaseio.com",
+    projectId: "whatsapp-mern-1d8b7",
+    storageBucket: "whatsapp-mern-1d8b7.appspot.com",
+    messagingSenderId: "412230942403",
+    appId: "1:412230942403:web:48d7a40217ff83c6236c76"
+  };
